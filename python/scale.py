9628859371839a86a43601c72b2b55361d7ed88a
Benjamin Landers <benjaminrlanders@gmail.com>
2013-12-17 23:00:17 -0800

    tested and fixed the script

--- python/scale.py
+++ python/scale.py
@@ -1,5 +1,6 @@
 from PIL import Image
+import sys
 img = Image.open(sys.argv[1])
-size = sys.argv[2], sys.argv[3]
+size = int(sys.argv[2]), int(sys.argv[3])
 out = img.resize(size)
 out.save(sys.argv[4])
