caea92557692b3dac83838c4daf8ed9368f66cea
Benjamin Landers <benjaminrlanders@gmail.com>
2016-12-13 01:08:32 -0800

    Add proof simplifier
    Its a relatively simple idea: make a graph of the dependencies of each inference (show lines depend on the critical bits of their insides) and the getting rid of everything that is not needed to support the main theorem.

--- javascript/theorem-prover.js
+++ javascript/theorem-prover.js
@@ -396,3 +396,106 @@ function toConditional(exp){
   lhs:toConditional(exp.lhs),rhs:toConditional(exp.rhs)};
 }
 var pl = s=>parse(lex(s));
+
+// For simplifying proofs (pruning unneeded deductions)
+function treeify(steps,i=0,graph=new Map(),lookup=new Map()){
+  var sub = new Set();
+  for(let step of steps){
+    i++;
+    let line = LINE(step);
+    line.i = i;
+    sub.add(line);
+    if(step.type === 'show'){
+      line.type = 'show';
+      [line.sub,_,i] = treeify(step.steps,i,graph,new Map(lookup));
+      lookup.set(str(step.exp),line);
+      graph.set(line,complete(line.sub,step.exp));
+      continue;
+    }
+    line.type = 'normal';
+    if(step.type === 'repetition'){
+      if(!lookup.has(str(step.exp))){
+        throw "NOOOOOOO";
+      }
+      graph.set(line,[lookup.get(str(step.exp))]);
+    }
+    if(step.type === 'assumption'){
+      lookup.set(str(step.exp),line);
+    }
+    if(step.type === 'derived'){
+      graph.set(line,step.from.map(a=>lookup.get(str(a))));
+      lookup.set(str(step.exp),line);
+    }
+  }
+  return [sub,graph,i-1];
+}
+function LINE(step){
+  return {step:step};
+}
+function contradiction(s){
+  for(let line of s.keys()){
+    let contradictions = [...s.keys()].filter(l2=>equal(l2.step.exp,NOT(line.step.exp)));
+    if(contradictions.length){
+      return [line,contradictions[0]];
+    }
+  }
+  throw "CONTRADICTION NOT FOUND";
+}
+function findExp(s,exp){
+  for(let line of s){
+    if(equal(line.step.exp,exp)){
+      return [line];
+    }
+  }
+  throw "EXP NOT FOUND";
+}
+function complete(s,exp){
+  if(exp.type === 'binary' && (exp.connective === '<->' || exp.connective === '^')){
+    return findExp(s,exp);
+  }
+  if(exp.type === 'binary' && exp.connective === '->'){
+    return findExp(s,exp.rhs);
+  }
+  return contradiction(s);
+}
+
+function flood(line,graph,out = new Set()){
+  out.add(line);
+  if(graph.has(line)){
+    for(let newline of graph.get(line)){
+      flood(newline,graph,out);
+    }
+  }
+  return out;
+}
+
+function filterProof(proof,lines){
+  for(let line of proof){
+
+    if(!lines.has(line)){
+      proof.delete(line);
+    }
+    if(line.type === 'show'){
+      filterProof(line.sub,lines);
+    }
+  }
+}
+
+function reformProof(proof){
+  var out = [];
+  for(let line of proof){
+    if(line.type === 'show'){
+      out.push({type:'show', exp: line.step.exp,steps:reformProof(line.sub)});
+    }else{
+      out.push(line.step);
+    }
+  }
+  return out;
+}
+
+function pruneSteps(steps){
+  var [s, graph] = treeify(steps);
+  var sub = flood([...s.keys()][0],graph);
+  filterProof(s,sub);
+  return reformProof(s)
+}
