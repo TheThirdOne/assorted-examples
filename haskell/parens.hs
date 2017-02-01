eb80890441c508f598b70ce9ec42fcf180ceed51
Benjamin Landers <benjaminrlanders@gmail.com>
2017-01-31 22:04:01 -0800

    Simplified and reformatted

--- haskell/parens.hs
+++ haskell/parens.hs
@@ -4,35 +4,34 @@ data TopEntry = P Paren | E Entry deriving (Show)
 data Entry = Tree Int [Entry] Int deriving (Show)
 type Paren = (Bool, Int)
 
--- | Inserts a Paren into paired parens and outputs the resulting list of paired parens and the one extra paren
-insertParen :: Entry -> Paren -> (Maybe Paren, [Entry], Maybe Paren)
+-- | Inserts a Paren into paired paren and outputs the resulting list of paired parens
+insertParen :: Entry -> Paren -> [Entry]
 insertParen (Tree left mid right) paren@(_, c)
-  | right < c       = (Nothing, [Tree left mid right], Just paren)
-  | c < left        = (Just paren, [Tree left mid right], Nothing)
-  | otherwise       = combine (partition mid paren) paren left right
+  | right < c || c < left        = [Tree left mid right]
+  | otherwise = combine (partition mid paren) paren left right
 
-deleteParen :: Entry -> Paren -> (Maybe Paren, [Entry], Maybe Paren)
+deleteParen :: Entry -> Paren -> [Entry]
 deleteParen (Tree left mid right) paren@(dir, c)
   | right < c || c < left       = error "Can't delete paren: not present"
-  | left == c && dir == False   = (Nothing, mid, Just (True,right))
-  | right == c && dir == True   = (Just (False,left), mid, Nothing)
+  | left == c && dir == False   = mid
+  | right == c && dir == True   = mid
   | otherwise                   = deleteHelper (partition mid paren) paren left right
 
-deleteHelper :: ([Entry], Maybe Entry, [Entry]) -> Paren -> Int -> Int -> (Maybe Paren, [Entry], Maybe Paren)
-deleteHelper (_, Nothing, _) _ _ _                               = error "Can't delete paren: not present"
-deleteHelper (xs, Just e@(Tree l _ _), ys) (True,c) left right   = (Just (False,left), xs++[Tree l (list++ys) right], Nothing)
-  where (_, list, _) = deleteParen e (True,c)
-deleteHelper (xs, Just e@(Tree _ _ r), ys) (False,c) left right  = (Nothing, (Tree left (xs++list) r):ys, Just (True, right))
-  where (_, list, _) = deleteParen e (False,c)
+deleteHelper :: ([Entry], Maybe Entry, [Entry]) -> Paren -> Int -> Int -> [Entry]
+deleteHelper (_, Nothing, _) _ _ _                           = error "Can't delete paren: not present"
+deleteHelper (xs, Just e@(Tree l _ _), ys) (True,c) _ right  = xs++[Tree l (list++ys) right]
+  where list = deleteParen e (True,c)
+deleteHelper (xs, Just e@(Tree _ _ r), ys) (False,c) left _  = (Tree left (xs++list) r):ys
+  where list = deleteParen e (False,c)
 
 -- | Recombines lists of entries and a paren, inserting the paren deeper if necessary
-combine :: ([Entry], Maybe Entry, [Entry]) -> Paren -> Int -> Int -> (Maybe Paren, [Entry], Maybe Paren)
-combine (xs, Nothing, ys) (True,c) left right                = (Nothing, (Tree left xs c):ys,         Just (True, right))
-combine (xs, Just e@(Tree _ _ r), ys) (True,c) left right    = (Nothing, (Tree left (xs++list) r):ys, Just (True, right))
-  where (_, list, _) = insertParen e (True,c)
-combine (xs, Nothing, ys) (False,c) left right               = (Just (False,left), xs++[Tree c ys right],         Nothing)
-combine (xs, Just e@(Tree l _ _), ys) (False,c) left right   = (Just (False,left), xs++[Tree l (list++ys) right], Nothing)
-  where (_, list, _) = insertParen e (False,c)
+combine :: ([Entry], Maybe Entry, [Entry]) -> Paren -> Int -> Int -> [Entry]
+combine (xs, Nothing, ys) (True,c) left _               = (Tree left xs c):ys
+combine (xs, Just e@(Tree _ _ r), ys) (True,c) left _   = (Tree left (xs++list) r):ys
+  where list= insertParen e (True,c)
+combine (xs, Nothing, ys) (False,c) _ right             = xs++[Tree c ys right]
+combine (xs, Just e@(Tree l _ _), ys) (False,c) _ right = xs++[Tree l (list++ys) right]
+  where list = insertParen e (False,c)
 
 -- | Partitions a list of entries into the ones on the left of a paren, around a paren and to the right of a paren
 partition :: [Entry] -> Paren -> ([Entry], Maybe Entry, [Entry])
@@ -84,9 +83,9 @@ deleteTopHelper (xs, Just (P paren@(dir,c)), ys) par@(d,i)  = if dir == d && c =
                                                               then (xs,Nothing,ys)
                                                               else error "Can't delete paren; opposite paren in index"
 deleteTopHelper (xs, Just (E e@(Tree l _ _)), ys) (True,c)  = (xs, Just (False,l),(map E list)++ys)
-  where (_, list, _) = deleteParen e (True,c)
+  where list = deleteParen e (True,c)
 deleteTopHelper (xs, Just (E e@(Tree _ _ r)), ys) (False,c) = (xs++(map E list), Just (True,r), ys)
-  where (_, list, _) = deleteParen e (False,c)
+  where list = deleteParen e (False,c)
 
 
 -- | Similar to combine, but prepares for linear probing instead of returning a list
@@ -94,9 +93,9 @@ combineTop :: ([TopEntry], Maybe TopEntry, [TopEntry]) -> Paren -> ([TopEntry],P
 combineTop (xs, Nothing, ys) paren                     = (xs,paren,ys)
 combineTop (_, Just (P _), _) _par                     = error "Can't insert a paren inside another paren"
 combineTop (xs, Just (E e@(Tree _ _ r)), ys) (True,c)  = (xs++(map E list), (True,r), ys)
-  where (_, list, _) = insertParen e (True,c)
+  where list = insertParen e (True,c)
 combineTop (xs, Just (E e@(Tree l _ _)), ys) (False,c) = (xs, (False, l), (map E list)++ys)
-  where (_, list, _) = insertParen e (False,c)
+  where list = insertParen e (False,c)
 
 -- | Same as partition, but for impure lists
 partitionTop :: [TopEntry] -> Paren -> ([TopEntry], Maybe TopEntry, [TopEntry])
@@ -125,8 +124,8 @@ convert :: Int -> [Char]
 convert i = ["\ESC[31m","\ESC[32m","\ESC[33m","\ESC[34m","\ESC[35m","\ESC[36m"] !! (i `mod` 6)
 
 printColored :: [TopEntry] -> [Char] -> [Char]
-printColored [] str  = "\ESC[39m"++str
-printColored _ []    = ""
+printColored [] str                        = "\ESC[39m"++str
+printColored _ []                          = ""
 printColored ((P (False,_)):xs) ('(':str)  = "\ESC[39m(" ++ (printColored xs str)
 printColored ((P (True,_)):xs)  (')':str)  = "\ESC[39m)" ++ (printColored xs str)
 printColored ((E tree):xs) str             = out++printColored xs remStr
@@ -134,13 +133,12 @@ printColored ((E tree):xs) str             = out++printColored xs remStr
 printColored xs (c:str) = c:(printColored xs str)
 
 printColored' :: [Entry] -> [Char] -> Int -> ([Char],[Char])
-printColored' [] str _ = (str,"")
-printColored' _ [] _ = ("","")
-printColored' ((Tree _ mid _):xs) ('(':str) i = (remStr, (convert i)++"(\ESC[39m"++out++tillClose++(convert i)++")"++"\ESC[39m"++out2)
-  where (afterMid, out) = printColored' mid str (i+1)
-        tillClose = takeWhile (/= ')') afterMid
-        afterClose = dropWhile (/= ')') afterMid
-        (remStr,out2) = printColored' xs (tail afterClose) i
-        
-printColored' xs (c:str) i = (remStr, c:out)
-  where (remStr, out) = printColored' xs str i
+printColored' [] str _                        = (str,"")
+printColored' _ []   _                        = ("","")
+printColored' ((Tree _ mid _):xs) ('(':str) i = (remStr, (convert i)++"(\ESC[39m"++elemOut++tillClose++(convert i)++")"++"\ESC[39m"++restOut)
+  where (afterMid, elemOut) = printColored' mid str (i+1)
+        tillClose           = takeWhile (/= ')') afterMid
+        afterClose          = dropWhile (/= ')') afterMid
+        (remStr,restOut)    = printColored' xs (tail afterClose) i
+printColored' xs (c:str) i                    = (remStr, c:out)
+  where (remStr, out)   = printColored' xs str i
