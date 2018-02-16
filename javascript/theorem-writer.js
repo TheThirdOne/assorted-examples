bf7b584dec85706432a4af31b678d694474b31ee
Benjamin Landers <benjaminrlanders@gmail.com>
2018-02-16 14:46:55 -0800

    Added the completeness theorem
    This required converting some of the prior theorems to conditional proofs for speed. If you want the full proofs for those theorems and don't know how to get them, check the last commit.

--- javascript/theorem-writer.js
+++ javascript/theorem-writer.js
@@ -143,8 +143,8 @@ MP('(~~~B->~B)','((~~~B->B)->~~B)'),
 Statement('(B->(~~~B->B))', 'Axiom (A1)'),
 ...corrallary1_10_a('B','(~~~B->B)','~~B')]);
 
-// |- ~B->(B->C)
-var lemma1_11_c = proof(['B','C'],Deduction('~B',Deduction('B',
+// ~B|-(B->C)
+var lemma1_11_c = proof(['B','C'],Deduction('B',
 [P('~B'),P('B'),
 Statement('(B->(~C->B))', 'Axiom (A1)'),
 Statement('(~B->(~C->~B))', 'Axiom (A1)'),
@@ -152,27 +152,33 @@ MP('B', '(~C->B)'),
 MP('~B', '(~C->~B)'),
 Statement('((~C->~B)->((~C->B)->C))', 'Axiom (A3)'),
 MP('(~C->~B)','((~C->B)->C)'),
-MP('(~C->B)','C')])));
+MP('(~C->B)','C')]));
 
-// |- (~C->~B)->(B->C)
-var contrapositive_d = proof(['B','C'],Deduction('(~C->~B)',
+// (~C->~B)|-(B->C)
+var contrapositive_d = proof(['B','C'],
 [P('(~C->~B)'),
 Statement('((~C->~B)->((~C->B)->C))', 'Axiom (A3)'),
 A1('B','~C'),
 MP('(~C->~B)','((~C->B)->C)'),
-...corrallary1_10_a('B','(~C->B)','C')]))
+...corrallary1_10_a('B','(~C->B)','C')]);
 
-// |- (B->C)->(~C->~B)
-var contrapositive_e = proof(['B','C'],Deduction('(B->C)',[P('(B->C)'),
+// (B->C)|-(~C->~B)
+var contrapositive_e = proof(['B','C'],
+[P('(B->C)'),
 ...double_negation_a('B'),
 ...corrallary1_10_a('~~B','B','C'),
 ...double_negation_b('C'),
 ...corrallary1_10_a('~~B','C','~~C'),
 ...contrapositive_d('~C','~B'),
-MP('(~~B->~~C)','(~C->~B)')]));
+MP('(~~B->~~C)','(~C->~B)')]);
+// B |- ~C -> ~(B->C)
+var lemma1_11_f = proof(['B','C'],[P('B'),
+...Deduction('(B->C)',[P('B'),P('(B->C)'),MP('B','C')]),
+...contrapositive_e('(B->C)','C')]);
 
-// |- (B->C)->((~B->C)->C)
-var separation_by_cases_f = proof(['B','C'],Deduction('(B->C)',Deduction('(~B->C)', [
+
+// (B->C),(~B->C)|-C)
+var separation_by_cases_g = proof(['B','C'],[
 P('(B->C)'),P('(~B->C)'),
 ...contrapositive_e('B','C'),
 MP('(B->C)','(~C->~B)'),
@@ -180,4 +186,99 @@ MP('(B->C)','(~C->~B)'),
 MP('(~B->C)','(~C->~~B)'),
 Statement('((~C->~~B)->((~C->~B)->C))', 'Axiom (A3)'),
 MP('(~C->~~B)','((~C->~B)->C)'),
-MP('(~C->~B)','C')])))
+MP('(~C->~B)','C')]);
+
+
+// Application of those base theorems into the completeness theorem follows
+
+// Parses a string starting at index i into an AST
+function parse(str,i=0){
+  if(i >= str.length)throw "Didn't finish parsing before string over";
+  if(str[i] == '('){
+    let [lhs,k] = parse(str,i+1);
+    if(str[k] != '-' && str[k+1] != '>' )throw "Expected -> got "+str[i];
+    let [rhs,j] = parse(str,k+2);
+    if(str[j] != ')') throw "Expected ) got " + str[j]; 
+    return [{type:'->',lhs,rhs},j+1];
+  }else if(str[i] == '~'){
+    let [rhs,k] = parse(str,i+1);
+    return [{type:'~',rhs},k];
+  }else {
+    let out = '';
+    while(i < str.length && str[i] != ')' && str[i] != '-'){
+         out+=str[i];
+         i++;
+    }
+    return [{type:'V', id:out},i];
+  }
+}
+
+// Evaluates an expression as always true (1), always false (-1), or conditionally true (0)
+function E(exp,truths=new Map()){
+  if(exp.type === 'V'){
+    if(!truths.has(exp.id))return 0;
+    return truths.get(exp.id);
+  }else if(exp.type === '~'){
+    return -E(exp.rhs,truths);
+  }else{
+        return Math.max(-E(exp.lhs,truths),E(exp.rhs,truths));
+    }
+    console.error('You shouldn\'t be here');
+}
+
+// Convert the ast to a string
+function exp2str(exp){
+  if(exp.type == 'V')return exp.id;
+  if(exp.type == '~')return '~' + exp2str(exp.rhs);
+  if(exp.type == '->')return '('+exp2str(exp.lhs) + '->' + exp2str(exp.rhs)+')';
+  throw "Should not get here, no more connectives";
+}
+
+// Prove a theorem (exp) using some variables given some prior truths (must be sentence letters 1 for true, -1 for false)
+function completeProve(exp,variables,truths = new Map()){
+  if(exp.type == '->'){
+    if(E(exp.rhs,truths) == 1){
+      let stmts = completeProve(exp.rhs,[],truths);
+      stmts.push(A1(exp2str(exp.rhs),exp2str(exp.lhs)));
+      stmts.push(MP(exp2str(exp.rhs),exp2str(exp)));
+      return stmts;
+    }else if(E(exp.lhs,truths) == -1){
+      let stmts = completeProve({type:'~',rhs:exp.lhs},[],truths);
+      stmts.push(...lemma1_11_c(exp2str(exp.lhs),exp2str(exp.rhs)));
+      return stmts;
+    }
+    let v = variables.pop();
+    truths.set(v,1);
+    let stmts = Deduction(v,completeProve(exp,variables,truths));
+    truths.set(v,-1);
+    stmts.push(...Deduction('~'+v,completeProve(exp,variables,truths)));
+    truths.delete(v);
+    variables.push(v);
+    stmts.push(...separation_by_cases_g(v,exp2str(exp)));
+    return stmts;
+  }
+  if(exp.type == '~'){
+    if(exp.rhs.type == '~'){
+      let stmts = completeProve(exp.rhs.rhs,variables,truths);
+      stmts.push(...double_negation_b(exp2str(exp.rhs.rhs)));
+      stmts.push(MP(exp2str(exp.rhs.rhs),exp2str(exp)));
+      return stmts;
+    }else if(exp.rhs.type == '->'){
+       let stmts = completeProve(exp.rhs.lhs,variables,truths);
+       stmts.push(...completeProve({type:'~',rhs:exp.rhs.rhs},variables,truths));
+       stmts.push(...lemma1_11_f(exp2str(exp.rhs.lhs),exp2str(exp.rhs.rhs)));
+       stmts.push(MP(exp2str({type:'~',rhs:exp.rhs.rhs}),exp2str(exp)));
+       return stmts;
+    }else{
+      if(E(exp,truths)!= 1)throw "Wat1";
+      return [P(exp2str(exp))];
+    }
+  }
+
+ if(exp.type == 'V'){
+   if(E(exp,truths)!= 1)throw "Wat2";
+   return [P(exp2str(exp))];
+ }
+ 
+ throw "shouldn't get here";
+}
